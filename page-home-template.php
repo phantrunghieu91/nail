ba83902c7e28d64758ac50fee397e7be041187ca
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-08-04 11:37:16 +0700

    UPDATE: Home page - Create home page template

---
+++ page-home-template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template name: JINS PAGE - Home page template
+ */
+get_template_part( 'gpw-templates/global/header' );
+
+get_template_part( 'gpw-templates/home-page/about-section' );
+
+get_template_part( 'gpw-templates/global/footer' );
