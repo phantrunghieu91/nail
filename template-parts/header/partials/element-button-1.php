b5145e921eba27ec1d6c648f54397d46dac78b55
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-08-11 09:08:19 +0700

    FIXED: header button 1 - Change icon position

--- template-parts/header/partials/element-button-1.php
+++ template-parts/header/partials/element-button-1.php
@@ -5,26 +5,27 @@
  * @package          Flatsome\Templates
  * @flatsome-version 3.19.0
  */
-$variant  = get_theme_mod('jins_header_button_variant', 'link');
-$size     = get_theme_mod('jins_header_button_size', 'extra-small');
+$variant  = get_theme_mod( 'jins_header_button_variant', 'link' );
+$size     = get_theme_mod( 'jins_header_button_size', 'extra-small' );
 $size     = $size !== 'extra-small' ? $size : '';
-$iconCode = get_theme_mod('jins_header_button_icon', '');
+$iconCode = get_theme_mod( 'jins_header_button_icon', '' );
 
 $buttonArgs = [
-  'label'   => get_theme_mod('jins_header_button_label', 'Jins button'),
-  'href'    => get_theme_mod('jins_header_button_link', ''),
-  'variant' => $variant,
-  'theme'   => get_theme_mod('jins_header_button_theme', 'primary'),
-  'size'    => $size,
-  'rounded' => get_theme_mod('jins_header_button_rounded', 'none'),
+  'label'         => get_theme_mod( 'jins_header_button_label', 'Jins button' ),
+  'href'          => get_theme_mod( 'jins_header_button_link', '' ),
+  'variant'       => $variant,
+  'theme'         => get_theme_mod( 'jins_header_button_theme', 'primary' ),
+  'size'          => $size,
+  'rounded'       => get_theme_mod( 'jins_header_button_rounded', 'none' ),
+  'icon_position' => 'left'
 ];
 
-if (!empty($iconCode) && $iconCode !== 'none') {
-    $buttonArgs['icon_code'] = $iconCode;
+if ( !empty( $iconCode ) && $iconCode !== 'none' ) {
+  $buttonArgs['icon_code'] = $iconCode;
 }
 ?>
 <li class="html header-button-1">
 	<div class="header-button">
-		<?php get_template_part('gpw-templates/global/jins-button', null, $buttonArgs); ?>
+		<?php get_template_part( 'gpw-templates/global/jins-button', null, $buttonArgs ); ?>
 	</div>
 </li>
