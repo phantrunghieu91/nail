15e954d2017db64ea09da80689e37200ae2332b6
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-08-20 14:11:13 +0700

    UPDATE: Pricing page - Changed general price design

--- gpw-templates/pricing-page/general-price-section.php
+++ gpw-templates/pricing-page/general-price-section.php
@@ -4,8 +4,43 @@
  * * Template: Pricing page - General price section
  */
 $sectionData = get_field( 'general_price' );
-if( empty( $sectionData['price'] ) ) {
+if( empty( $sectionData['service_group'] ) ) {
   return;
+}
+function displayService( $serviceData ) {
+  ?>
+  <article class="general-price__services" aria-orientation="<?= esc_attr( $serviceData['display_style'] ) ?>">
+    <?php if( !empty( $serviceData['image'] ) ) {
+      echo wp_get_attachment_image( $serviceData['image'], 'large', false, [ 'class' => 'general-price__services-image', 'alt' => $serviceData['name']] );
+    } ?>
+    <div class="general-price__services-content">
+      <?php if( !empty( $serviceData['name'] ) ): ?>
+        <h4 class="general-price__services-name"><?= esc_html( $serviceData['name'] ) ?></h4>
+      <?php endif ?>
+      <?php if( !empty( $serviceData['description'] ) ): ?>
+        <div class="general-price__services-desc"><?= wp_kses_post( $serviceData['description'] ) ?></div>
+      <?php endif ?>
+      <?php if( !empty( $serviceData['table'] ) ): ?>
+        <ul class="general-price__services-list">
+          <?php foreach( $serviceData['table'] as $item ) : ?>
+            <li class="general-price__service">
+              <?php if( !empty( $item['name'] ) ) :?>
+                <h5 class="general-price__service-name"><?= esc_html( $item['name' ] ) ?></h5>
+              <?php endif ?>
+              <?php if( !empty( $item['description'] ) ) :?>
+                <div class="general-price__service-description"><?= wp_kses_post( $item['description' ] ) ?></div>
+              <?php endif ?>
+              <?php if( !empty( $item['price'] ) ) {
+                echo jins_render_price( $item['price'], 'general-price__service' );
+              } ?>
+              
+            </li>
+          <?php endforeach ?>
+        </ul>
+      <?php endif ?>
+    </div>
+  </article>
+  <?php
 }
 ?>
 <section class="general-price">
@@ -19,39 +54,19 @@ if( empty( $sectionData['price'] ) ) {
     </header>
     <?php endif ?>
     <main class="general-price__main">
-      <?php foreach( $sectionData['price'] as $price ) :
-        if( empty( $price['table'] ) ) continue;
-        $serviceID = sanitize_title( $price['name'] );
+      <?php foreach( $sectionData['service_group'] as $serviceGroup ) :
+        if( empty( $serviceGroup['price'] ) ) continue;
+        $serviceGroupID = sanitize_title( $serviceGroup['title'] );
         ?>
-        <article class="general-price__services" id="<?= esc_attr( $serviceID ) ?>" aria-orientation="<?= esc_attr( $price['display_style'] ) ?>">
-          <?php if( !empty( $price['image'] ) ) {
-            echo wp_get_attachment_image( $price['image'], 'large', false, [ 'class' => 'general-price__services-image', 'alt' => $price['name']] );
+        <div class="general-price__services-group" id="<?= esc_html( $serviceGroupID ) ?>">
+          <?php if( true === $serviceGroup['show_group_title'] ) : ?>
+            <h3 class="general-price__services-group-title"><?= esc_html( $serviceGroup['title'] ) ?></h3>
+          <?php endif ?>
+          <?php foreach( $serviceGroup['price'] as $price ) {
+            if( empty( $price ) ) continue;
+            displayService( $price );
           } ?>
-          <div class="general-price__services-content">
-            <?php if( !empty( $price['name'] ) ): ?>
-              <h3 class="general-price__services-name"><?= esc_html( $price['name'] ) ?></h3>
-            <?php endif ?>
-            <?php if( !empty( $price['description'] ) ): ?>
-              <div class="general-price__services-desc"><?= esc_html( $price['description'] ) ?></div>
-            <?php endif ?>
-            <ul class="general-price__services-list">
-              <?php foreach( $price['table'] as $item ) : ?>
-                <li class="general-price__service">
-                  <?php if( !empty( $item['name'] ) ) :?>
-                    <h4 class="general-price__service-name"><?= esc_html( $item['name' ] ) ?></h4>
-                  <?php endif ?>
-                  <?php if( !empty( $item['description'] ) ) :?>
-                    <div class="general-price__service-description"><?= esc_html( $item['description' ] ) ?></div>
-                  <?php endif ?>
-                  <?php if( !empty( $item['price'] ) ) {
-                    echo jins_render_price( $item['price'], 'general-price__service' );
-                  } ?>
-                  
-                </li>
-              <?php endforeach ?>
-            </ul>
-          </div>
-        </article>
+        </div>
       <?php endforeach ?>
     </main>
   </div>
