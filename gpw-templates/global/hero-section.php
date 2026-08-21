68f44ded24735c776699a2e36c0288b25b566fec
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-08-21 10:26:54 +0700

    UPDATE: Global - hero section

--- gpw-templates/global/hero-section.php
+++ gpw-templates/global/hero-section.php
@@ -10,5 +10,9 @@ if( empty( $sectionData['banner'] )) {
 ?>
 <section class="hero">
   <?= wp_get_attachment_image( $sectionData['banner'], 'full', false, ['class' => 'hero__bg', 'alt' => 'Hero banner']) ?>
-  <div class="section__inner"></div>
+  <div class="section__inner">
+    <?php if( true === $sectionData['show_page_title'] ) : ?>
+      <h1 class="hero__title"><?= get_the_title() ?></h1>
+    <?php endif ?>
+  </div>
 </section>
