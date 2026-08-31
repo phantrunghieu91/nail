fcac293ede853d5c432a8c458694a31d53ecf536
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-08-31 11:13:30 +0700

    FIXED: About page - Introduction section

--- gpw-templates/about-page/introduction-section.php
+++ gpw-templates/about-page/introduction-section.php
@@ -1,32 +1,28 @@
-<?php 
+<?php
 /**
  * @author Hieu "Jin" Phan Trung
  * * Template: About page - Introduction section
  */
-$sectionData = get_field('introduction');
-if( empty( $sectionData )) {
+$sectionData = get_field( 'introduction' );
+if( empty( $sectionData['content'] ) ) {
   return;
 }
 ?>
 <section class="introduction">
   <div class="section__inner">
-    <?php foreach( $sectionData as $block ) : ?>
-      <div class="introduction__block">
-        <?php if( !empty( $block['image'] )) {
-          echo wp_get_attachment_image( $block['image'], 'large', false, [ 'class' => 'introduction__block-image', ]);
-        } ?>
-        <div class="introduction__block-content">
-          <?php if( !empty( $block['sub_title'] )) : ?>
-            <span class="section__sub-title"><?= esc_html( $block['sub_title'] ) ?></span>
-          <?php endif ?>
-          <?php if( !empty( $block['title'] )) : ?>
-            <h2 class="section__title"><?= esc_html( $block['title'] ) ?></h2>
-          <?php endif ?>
-          <?php if( !empty( $block['content'] )) : ?>
-            <div class="section__content"><?= wp_kses_post( $block['content'] ) ?></div>
-          <?php endif ?>
-        </div>
-      </div>
-    <?php endforeach ?>
+    <?php if( !empty( $sectionData['image'] ) ) {
+      echo wp_get_attachment_image( $sectionData['image'], 'large', false, [ 'class' => 'introduction__image', ] );
+    } ?>
+    <div class="introduction__content-wrapper">
+      <?php if( !empty( $sectionData['sub_title'] ) ) : ?>
+        <span class="section__sub-title section__sub-title--center"><?= esc_html( $sectionData['sub_title'] ) ?></span>
+      <?php endif ?>
+      <?php if( !empty( $sectionData['title'] ) ) : ?>
+        <h2 class="section__title section__title--center"><?= esc_html( $sectionData['title'] ) ?></h2>
+      <?php endif ?>
+      <?php if( !empty( $sectionData['content'] ) ) : ?>
+        <div class="section__content section__content--center"><?= wp_kses_post( $sectionData['content'] ) ?></div>
+      <?php endif ?>
+    </div>
   </div>
 </section>
